f2dce9ceaeab84019f731e9a4c508855a0df3c95
Christian Hofmann <hofiorg@googlemail.com>
2018-10-22 18:02:00 +0200

    2 new examples

--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import MyButton from "./MyButton";
+import Clock from "./Clock";
 
 class Hello extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      counter: 0,
       value: ['Eins', 'Zwei', 'Drei', 'Vier', 'Fünf', 'Sechs', 'Sieben', 'Acht', 'Neun'],
     };
   }
@@ -13,12 +16,21 @@ class Hello extends Component {
       .filter(s => s.startsWith('S'))
       .map((s) => <li key={s}>{s}</li>);
 
+  clickFunction = () => {
+    this.setState((state) => ({
+      counter: state.counter + 1
+    }));
+  };
+
   render() {
     return (
       <div>
         <p>
-          Hello {this.props.name}!
+          Hello {this.props.name}! Counter: {this.state.counter}
         </p>
+        <Clock/>
+        <MyButton text='Test' onclick={this.clickFunction}/>
+        <br/>
         <ul>{this.createList(this.state.value)}</ul>
       </div>
     );
---
+++ src/MyButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MyButton extends Component {
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.onclick} type="button" className="btn btn-outline-primary">{this.props.text}</button>
+      </div>
+    );
+  }
+}
+
+export default MyButton;
